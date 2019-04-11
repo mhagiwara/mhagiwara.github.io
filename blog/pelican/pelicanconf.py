3f213ca4ff702d68c2ad614bc4b76ca5515c53fb
Masato Hagiwara <hagisan@gmail.com>
2019-04-11 16:28:50 -0400

    Add blog; redirect makijita article

---
+++ blog/pelican/pelicanconf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Masato Hagiwara'
+SITEURL = 'http://localhost:8000'
+SITENAME = u'米国で働く機械学習エンジニアのブログ'
+SITETITLE = u'米国で働く機械学習エンジニアのブログ'
+SITESUBTITLE = u'米国でフリーの機械学習・自然言語処理エンジニア・コンサルタントとして働いています。機械学習・自然言語処理、英語・中国語学習などのトピックが中心です。'
+SITEDESCRIPTION = SITESUBTITLE
+SITELOGO = 'http://masatohagiwara.net/face.jpg'
+
+THEME = './Flex'
+PATH = 'content'
+TIMEZONE = 'America/New_York'
+
+I18N_TEMPLATES_LANG = 'ja'
+DEFAULT_LANG = 'ja'
+OG_LOCALE = 'ja_JP'
+LOCALE = 'ja_JP.UTF-8'
+
+DATE_FORMATS = {
+    'en': '%B %d, %Y',
+    'ja': u'%Y-%m-%d(%a)',
+}
+
+DISABLE_URL_HASH = True
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('ブログトップ', 'http://masatohagiwara.net/blog/'),
+         ('著者個人ページ', 'http://masatohagiwara.net/'))
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/mhagiwara'),
+          ('twitter', 'https://twitter.com/mhagiwara_ja'),
+          ('rss', 'feeds/all.atom.xml'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
