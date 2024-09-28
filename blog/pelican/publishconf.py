3a06b9818b222e571007b5345426276e4e4a0bd5
Masato Hagiwara <hagisan@gmail.com>
2024-09-27 17:24:05 -0700

    Update blog

--- blog/pelican/publishconf.py
+++ blog/pelican/publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'http://masatohagiwara.net/blog'
+SITEURL = 'https://masatohagiwara.net/blog'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
